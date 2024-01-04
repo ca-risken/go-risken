2d41a65df7c70967c242d1cabd2b3a58efb3c8e9
gassara-kys <ogasawara_kiyoshi@cybernagent>
2024-01-05 01:15:36 +0900

    add: test

--- client_test.go
+++ client_test.go
@@ -88,3 +88,81 @@ func TestCheckResponse(t *testing.T) {
 		})
 	}
 }
+
+func TestStructToQueryParams(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "struct to query params",
+			args: args{
+				v: struct {
+					ProjectID uint32  `json:"project_id"`
+					Email     string  `json:"email"`
+					Password  string  `json:"password"`
+					Test      bool    `json:"test"`
+					Score     float32 `json:"score"`
+				}{
+					ProjectID: 1,
+					Email:     "alice@example.com",
+					Password:  "password",
+					Test:      true,
+					Score:     0.5,
+				},
+			},
+			want: "email=alice%40example.com&password=password&project_id=1&score=0.5&test=true",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := structToQueryParams(tt.args.v).Encode()
+			if got != tt.want {
+				t.Errorf("structToQueryParams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStructToJSONRequestBody(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "struct to JSON request body",
+			args: args{
+				v: struct {
+					ProjectID uint32  `json:"project_id"`
+					Email     string  `json:"email"`
+					Password  string  `json:"password"`
+					Test      bool    `json:"test"`
+					Score     float32 `json:"score"`
+				}{
+					ProjectID: 1,
+					Email:     "bob@example.com",
+					Password:  "password",
+					Test:      true,
+					Score:     0.5,
+				},
+			},
+			want: `{"project_id":1,"email":"bob@example.com","password":"password","test":true,"score":0.5}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := structToJSONRequestBody(tt.args.v)
+			if string(got) != tt.want {
+				t.Errorf("structToJSONRequestBody() = %v, want %v", string(got), tt.want)
+			}
+		})
+	}
+}
