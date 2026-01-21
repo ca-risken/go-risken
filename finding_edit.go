aee814985d83b82bde9ce12e5325f788de8c2916
Senk <44151180+senk8@users.noreply.github.com>
2026-01-21 14:17:31 +0900

    Add organization API support and ListFindingForOrg method

--- finding_edit.go
+++ finding_edit.go
@@ -36,3 +36,34 @@ func (c *Client) ListFinding(ctx context.Context, req *proto.ListFindingRequest)
 	}
 	return &resp, nil
 }
+
+func (c *Client) ListFindingForOrg(ctx context.Context, req *proto.ListFindingForOrgRequest) (*proto.ListFindingForOrgResponse, error) {
+	httpReq, err := c.NewRequest(ctx, "GET", "/api/v1/finding/list-finding-for-organization", req)
+	if err != nil {
+		return nil, err
+	}
+
+	// クエリを上書き
+	query := httpReq.URL.Query()
+	switch req.Status {
+	case proto.FindingStatus_FINDING_UNKNOWN:
+		query.Set("status", "0") // all
+	case proto.FindingStatus_FINDING_ACTIVE:
+		query.Set("status", "1") // active
+	case proto.FindingStatus_FINDING_PENDING:
+		query.Set("status", "2") // pending
+	default:
+		query.Set("status", "1") // デフォルトはactive
+	}
+	httpReq.URL.RawQuery = query.Encode()
+
+	httpResp, err := c.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	var resp proto.ListFindingForOrgResponse
+	if err := decodeBodyWithDataKey(httpResp, &resp); err != nil {
+		return nil, err
+	}
+	return &resp, nil
+}
